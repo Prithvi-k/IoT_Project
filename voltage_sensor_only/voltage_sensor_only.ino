10e200a1171e9e5f76972863f1e29347d836e7d7
Prithvi-k <79378970+Prithvi-k@users.noreply.github.com>
2023-05-27 19:53:21 +0530

    Calibrated Voltage sensors

--- voltage_sensor_only/voltage_sensor_only.ino
+++ voltage_sensor_only/voltage_sensor_only.ino
@@ -8,23 +8,12 @@ void setup()
 void loop() 
 {
   ADC_VALUE = analogRead(analog_channel_pin);
-  // Serial.println(ADC_VALUE);
+  Serial.println(ADC_VALUE);
   // Serial.print("ADC VALUE = ");
   // Serial.println(ADC_VALUE);
   // delay(1000);
   // float voltage_value = (ADC_VALUE * 3.3 ) / (4095.0);
-  float v=(ADC_VALUE*5000)/5860.0;
-        float cur=v-2500;//2470.345;
-        cur/=1000;
-  // Serial.print("Current = ");
-  // Serial.println(cur);
-  // Serial.print(" Amphere");
-  // delay(1000);
-  // float current=(ADC_VALUE*5)/1023.0;
-  // current=(current-8.96)/0.185;
-  Serial.println(cur);
-
-  // float current=ADC_VALUE-2930;
-  // current=current*5/4905
+  float v=(ADC_VALUE)*(ADC_VALUE*-9.66631*1e-7+0.0054736);
+  Serial.println(v);
   delay(1000);
 }
