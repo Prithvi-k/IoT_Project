6857374e2e8b33d8a8b18bf39fa0ff4a0f8a5f2d
tanaygad <127509671+tanaygad@users.noreply.github.com>
2023-05-29 01:02:23 +0530

    Update IoTProject.ino

--- IoTProject.ino
+++ IoTProject.ino
@@ -13,6 +13,7 @@
 #include "ThingSpeak.h"
 #include <HTTPClient.h>
 int master_count = 0;
+int thingspeakcount=0;
 Adafruit_BMP280 bmp; // use I2C interface
 int count = 0;
 float sum_t = 0, sum_p = 0, sum_a = 0, mean_t = 0, mean_p = 0, mean_a = 0;
@@ -24,6 +25,29 @@ int voltage_value = 0;
 char* SSID="Redmi 9 Prime";
 char* pass="ameya3103";
 WiFiClient client;
+#define CSE_IP      "192.168.43.184"
+#define CSE_PORT    5089
+#define HTTPS     false
+#define OM2M_ORGIN    "admin:admin"
+#define OM2M_MN     "/~/in-cse/in-name/"
+#define OM2M_AE     "SOLAR-OPTIMISATION"
+#define OM2M_DATA_CONT  "Node-1/Data"
+
+const char* ntpServer = "pool.ntp.org";
+unsigned long epochTime; 
+long randNumber;
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    //Serial.println("Failed to obtain time");
+    return(0);
+  }
+  time(&now);
+  return now;
+}
+
+HTTPClient http;
 //defining Servos
 Servo servohori;
 int servoh = 90;
@@ -39,12 +63,9 @@ int ldrtopl = 34; //top left LDR green
 int ldrtopr = 33; //top right LDR yellow
 int ldrbotl = 35; // bottom left LDR blue
 int ldrbotr = 32; // bottom right LDR orange
-int ameya;
-int tanay;
 void setup() {
   // put your setup code here, to run once:
-  ameya=0;
-  tanay=0;
+configTime(0, 0, ntpServer);
   bmp.begin(0x76);
  Serial.begin(9600);
   Serial.println("Connecting to WiFi...");
@@ -69,12 +90,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+   epochTime = getTime();
   count++;
   master_count++;
-
-  if(master_count == 10)
-  {
-
+  int occupancy=0;
+  thingspeakcount++;
     float temp = bmp.readTemperature();
     sum_t = sum_t + temp;
     mean_t = sum_t/count;
@@ -106,11 +126,21 @@ void loop() {
     Serial.print("Voltage: ");
     Serial.println(v);
 
-    // float current=ADC_VALUE-2930;
-    // current=current*5/4905
-    master_count = 0;
+  Serial.println("!");
+  int adc=analogRead(voltage_analog_channel_pin);
+  // float v=adc*5000/5800.0;
+  float cur=v-2500;//2470.345;
+  cur/=185;
+  cur=-cur;
+  if(thingspeakcount==8){
+  ThingSpeak.setField(1,temp);
+  ThingSpeak.setField(2,pressure);
+  ThingSpeak.setField(3,altitude);
+  ThingSpeak.setField(4,cur);
+  ThingSpeak.setField(5,v);
+  ThingSpeak.writeFields(Channel_ID,Channel_API_Key);
+    thingspeakcount=0;
   }
-
   servoh = servohori.read();
   servov = servoverti.read();
   //capturing analog values of each LDR
@@ -184,12 +214,138 @@ void loop() {
   }
   Serial.println("");
   Serial.println("");
+  static int i=0;
+  String data="[" + String(epochTime) + ", " + String(occupancy) + " , " + String(temp)+"]";
 
+String server="http://" + String() + CSE_IP + ":" + String() + CSE_PORT + String()+OM2M_MN;
+
+Serial.println(data);
+http.begin(server + String() +OM2M_AE + "/" + "Temp/Data" + "/");
+
+http.addHeader("X-M2M-Origin", OM2M_ORGIN);
+http.addHeader("Content-Type", "application/json;ty=4");
+http.addHeader("Content-Length", "100");
+
+String label = "Temp";
+
+String req_data = String() + "{\"m2m:cin\": {"
+
+  + "\"con\": \"" + data + "\","
+
+  + "\"rn\": \"" + "cin_"+String(i) + "\","
+
+  + "\"lbl\": \"" + label + "\","
+
+  + "\"cnf\": \"text\""
+
+  + "}}";
+int code = http.POST(req_data);
+http.end();
+Serial.println(code);
+data="[" + String(epochTime) + ", " + String(occupancy) + " , " + String(pressure)+"]";
+
+server="http://" + String() + CSE_IP + ":" + String() + CSE_PORT + String()+OM2M_MN;
+
+Serial.println(data);
+http.begin(server + String() +OM2M_AE + "/" + "Pressure/Data" + "/");
+
+http.addHeader("X-M2M-Origin", OM2M_ORGIN);
+http.addHeader("Content-Type", "application/json;ty=4");
+http.addHeader("Content-Length", "100");
+
+label = "Pressure";
+req_data = String() + "{\"m2m:cin\": {"
+
+  + "\"con\": \"" + data + "\","
+
+  + "\"rn\": \"" + "cin_"+String(i++) + "\","
+
+  + "\"lbl\": \"" + label + "\","
+
+  + "\"cnf\": \"text\""
+
+  + "}}";
+code = http.POST(req_data);
+http.end();
+Serial.println(code);
+data="[" + String(epochTime) + ", " + String(occupancy) + " , " + String(altitude)+"]";
+
+server="http://" + String() + CSE_IP + ":" + String() + CSE_PORT + String()+OM2M_MN;
+
+Serial.println(data);
+http.begin(server + String() +OM2M_AE + "/" + "Humidity/Data" + "/");
+
+http.addHeader("X-M2M-Origin", OM2M_ORGIN);
+http.addHeader("Content-Type", "application/json;ty=4");
+http.addHeader("Content-Length", "100");
+
+label = "Humidity";
+
+req_data = String() + "{\"m2m:cin\": {"
+
+  + "\"con\": \"" + data + "\","
+
+  + "\"rn\": \"" + "cin_"+String(i++) + "\","
+
+  + "\"lbl\": \"" + label + "\","
+
+  + "\"cnf\": \"text\""
+
+  + "}}";
+code = http.POST(req_data);
+http.end();
+Serial.println(code);
+data="[" + String(epochTime) + ", " + String(occupancy) + " , " + String(cur)+"]";
+
+server="http://" + String() + CSE_IP + ":" + String() + CSE_PORT + String()+OM2M_MN;
+
+Serial.println(data);
+http.begin(server + String() +OM2M_AE + "/" + "Current-1/Data" + "/");
+
+http.addHeader("X-M2M-Origin", OM2M_ORGIN);
+http.addHeader("Content-Type", "application/json;ty=4");
+http.addHeader("Content-Length", "100");
+
+label = "Current-1";
+req_data = String() + "{\"m2m:cin\": {"
+
+  + "\"con\": \"" + data + "\","
+
+  + "\"rn\": \"" + "cin_"+String(i++) + "\","
+
+  + "\"lbl\": \"" + label + "\","
+
+  + "\"cnf\": \"text\""
+
+  + "}}";
+code = http.POST(req_data);
+http.end();
+Serial.println(code);
+data="[" + String(epochTime) + ", " + String(occupancy) + " , " + String(v)+"]";
+
+server="http://" + String() + CSE_IP + ":" + String() + CSE_PORT + String()+OM2M_MN;
+
+Serial.println(data);
+http.begin(server + String() +OM2M_AE + "/" + "Voltage-1/Data" + "/");
+
+http.addHeader("X-M2M-Origin", OM2M_ORGIN);
+http.addHeader("Content-Type", "application/json;ty=4");
+http.addHeader("Content-Length", "100");
+label = "Voltage-1";
+
+req_data = String() + "{\"m2m:cin\": {"
+
+  + "\"con\": \"" + data + "\","
+
+  + "\"rn\": \"" + "cin_"+String(i++) + "\","
+
+  + "\"lbl\": \"" + label + "\","
+
+  + "\"cnf\": \"text\""
+
+  + "}}";
+code = http.POST(req_data);
+http.end();
+Serial.println(code);
   delay(50);
-  ameya+=34;
-  tanay+=56;
-  ThingSpeak.setField(1,ameya);
-  ThingSpeak.setField(2,tanay);
-  ThingSpeak.writeFields(Channel_ID,Channel_API_Key);
-  delay(15000);
 }
